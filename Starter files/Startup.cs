354a43650ba940c92302879c4d4699d0bec0561a
Kevin Dockx <kevin.dockx@gmail.com>
2019-10-04 17:53:44 +0200

    Update starter files

--- Starter files/Startup.cs
+++ Starter files/Startup.cs
@@ -22,11 +22,7 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers(setupAction =>
-            {
-                setupAction.ReturnHttpNotAcceptable = true;
-                
-            }).AddXmlDataContractSerializerFormatters();
+           services.AddControllers();
              
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
